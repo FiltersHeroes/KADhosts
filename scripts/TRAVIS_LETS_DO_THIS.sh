64dd8ad55971e551c7b180ea00472c0f44a65b71
hawkeye116477 <hawkeye116477@gmail.com>
2019-04-06 18:15:06 +0200

    Porządki [ci skip]

--- scripts/TRAVIS_LETS_DO_THIS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Sciezka to miejsce, w którym znajduje się skrypt
-sciezka=$(dirname "$0")
-
-cd $sciezka/..
-
-$sciezka/VICHS_Travis.sh $sciezka/../KADhosts.txt
--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -8,7 +8,17 @@ cd $sciezka/..
 if [ "$CI" = "true" ] ; then
     git config --global user.email "PolishJarvis@int.pl"
     git config --global user.name "PolishJarvis"
+    ost_plik=$(git diff-tree --no-commit-id --name-only -r master)
+else
+    ost_plik=$(git diff -z --name-only | xargs -0)
 fi
 
+if [[ "$ost_plik" == *"sections"* ]]; then
+    if [[ "$lista" != *" KADhosts.txt"* ]] ;then
+        lista+=" "KADhosts.txt
+    fi
+fi
 
-$sciezka/VICHS.sh $sciezka/../KADhosts.txt
+if [ "$lista" ] ; then
+    $sciezka/VICHS.sh $lista
+fi
