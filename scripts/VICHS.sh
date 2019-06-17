42bf52ffde1606e7c386054a51ba348d20930a71
hawkeye116477 <hawkeye116477@gmail.com>
2019-06-17 22:10:32 +0200

    Aktualizacja skryptu i poprawka do listy [ci skip]

--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # VICHS - Version Include Checksum Hosts Sort
-# v2.3.5
+# v2.3.6
 
 # MAIN_PATH to miejsce, w którym znajduje się główny katalog repozytorium (zakładamy, że skrypt znajduje się w katalogu o 1 niżej od głównego katalogu repozytorium)
 MAIN_PATH=$(dirname "$0")/..
@@ -173,7 +173,7 @@ for i in "$@"; do
         sed -i "s|[|][|]|0.0.0.0 |" "$EXTERNALHOSTS_TEMP"
         sed -i 's/[/\^]//g' "$EXTERNALHOSTS_TEMP"
         sed -i '/[/\*]/d' "$EXTERNALHOSTS_TEMP"
-        sed -r "/(www\.|www[0-9]\.|www\-|pl\.)/! s/^0\.0\.0\.0 /0.0.0.0 www./" "$EXTERNALHOSTS_TEMP" > "$EXTERNALHOSTS_TEMP.2"
+        sed -r "/(www\.|www[0-9]\.|www\-|pl\.| [0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9])/! s/^0\.0\.0\.0 /0.0.0.0 www./" "$EXTERNALHOSTS_TEMP" > "$EXTERNALHOSTS_TEMP.2"
         if [ -f "$EXTERNALHOSTS_TEMP.2" ]
         then
             cat "$EXTERNALHOSTS_TEMP" "$EXTERNALHOSTS_TEMP.2"  > "$EXTERNALHOSTS_TEMP.3"
