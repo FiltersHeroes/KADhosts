163e285dc8ef1c1e777cacf5798df0146126d2af
hawkeye116477 <hawkeye116477@gmail.com>
2019-11-17 22:15:28 +0100

    Aktualizacja skryptu [ci skip]

--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # VICHS - Version Include Checksum Hosts Sort
-# v2.8.9
+# v2.9.0
 
 # MIT License
 
@@ -270,6 +270,7 @@ for i in "$@"; do
     done
 
     function convertToHosts() {
+        sed -i "s|\$all$||" "$1"
         sed -i "s|[|][|]|0.0.0.0 |" "$1"
         sed -i 's/[\^]//g' "$1"
         sed -i '/[/\*]/d' "$1"
@@ -287,6 +288,7 @@ for i in "$@"; do
         HOSTS_TEMP=$SECTIONS_DIR/hosts.temp
         grep -o '\||.*^$' "$HOSTS_FILE" > "$HOSTS_TEMP"
         grep -o '\0.0.0.0.*' "$HOSTS_FILE" >> "$HOSTS_TEMP"
+        grep -o '\||.*^$all$' "$HOSTS_FILE" >> "$HOSTS_TEMP"
         convertToHosts "$HOSTS_TEMP"
         if [ -f "$HOSTS_TEMP.2" ]
         then
@@ -315,6 +317,7 @@ for i in "$@"; do
         wget -O "$EXTERNAL_TEMP" "${EXTERNAL}"
         revertWhenDownloadError
         grep -o '\||.*^$' "$EXTERNAL_TEMP" > "$EXTERNALHOSTS_TEMP"
+        grep -o '\||.*^$all$' "$EXTERNAL_TEMP" > "$EXTERNALHOSTS_TEMP"
         convertToHosts "$EXTERNALHOSTS_TEMP"
         if [ -f "$EXTERNALHOSTS_TEMP.2" ]
         then
@@ -355,6 +358,7 @@ for i in "$@"; do
         PH_FILE=${SECTIONS_DIR}/$(grep -oP -m 1 '@PHinclude \K.*' "$FINAL").txt
         PH_TEMP=$SECTIONS_DIR/ph.temp
         grep -o '\||.*^$' "$PH_FILE" > "$PH_TEMP"
+        grep -o '\||.*^$all$' "$PH_FILE" > "$PH_TEMP"
         convertToPihole "$PH_TEMP"
         sort -uV -o "$PH_TEMP" "$PH_TEMP"
         sed -e '0,/^@PHinclude/!b; /@PHinclude/{ r '"$PH_TEMP"'' -e 'd }' "$FINAL" > "$TEMPORARY"
@@ -390,6 +394,7 @@ for i in "$@"; do
         wget -O "$EXTERNAL_TEMP" "${EXTERNAL}"
         revertWhenDownloadError
         grep -o '\||.*^$' "$EXTERNAL_TEMP" > "$EXTERNALPH_TEMP"
+        grep -o '\||.*^$all$' "$EXTERNAL_TEMP" > "$EXTERNALPH_TEMP"
         convertToPihole "$EXTERNALPH_TEMP"
         sort -uV -o "$EXTERNALPH_TEMP" "$EXTERNALPH_TEMP"
         sed -e '0,/^@URLPHinclude/!b; /@URLPHinclude/{ r '"$EXTERNALPH_TEMP"'' -e 'd }' "$FINAL" > "$TEMPORARY"
